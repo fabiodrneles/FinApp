83a73cbba4c954bc33898877db5a942505c17572
fabiodrneles <fabiodrneles@gmail.com>
2024-07-22 19:26:17 -0300

    criando app finance, configurando o ambiente

---
+++ src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Header from '../../components/Header';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Header/>
+      <Text>Pagina Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
